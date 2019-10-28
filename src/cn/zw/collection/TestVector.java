3aeba1eb6aae6b09b328949d4c56606549645cf7
Administrator <Administrator@192.168.41.1>
2019-10-28 19:17:30 +0800

    Vector

---
+++ src/cn/zw/collection/TestVector.java
@@ -0,0 +1,16 @@
+package cn.zw.collection;
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * ≤‚ ‘vector
+ * @author Administrator
+ *
+ */
+
+public class TestVector {
+	public static void main(String[] args) {
+		List a = new Vector();
+	}
+}
